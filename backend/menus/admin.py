6357549f4c27c84011ba5d09e33c34c2dc3e6a7f
john <izolotavin99@gmail.com>
2022-12-29 10:47:32 +0300

    1) menus.admin. MenuItem added 2) manage.py initadmin added

--- backend/menus/admin.py
+++ backend/menus/admin.py
@@ -1,3 +1,20 @@
+import nested_admin
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import MenuItem
+
+
+class MenuItemInline(nested_admin.NestedStackedInline):
+    model = MenuItem
+    extra = 3
+
+
+class MenuItemAdmin(nested_admin.NestedModelAdmin):
+    inlines = [MenuItemInline]
+
+
+admin.site.register(MenuItem, MenuItemAdmin)
+
+
+admin.site.register(MenuItem, MenuItemAdmin)
---
+++ backend/menus/management/commands/initadmin.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Создает админа (root:root)'
+    def handle(self, *args, **options):
+        """Создает админа, если в БД нет ни одного user(is_superuser=True)"""
+
+        if not User.objects.filter(is_superuser=True):
+            User.objects.create_superuser(
+                username='root',
+                password='root',
+            )
+
+        self.stdout.write(self.style.SUCCESS("Админ создан!"))
